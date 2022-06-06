814b8e4dee4a60916462f8b40739bbe1e6d9c50a
Matías Ruiz <ma.ruizsa@duocuc.cl>
2022-06-06 18:29:57 -0400

    cache
    cache

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import home
+from .views import home, macetero
+from .views import contacto
+from .views import flores
+from .views import macetero
+from .views import tierradehoja
 
 urlpatterns = [
     path('', home,name="home"),
+    path('contacto.html', contacto,name="contacto"),
+    path('flores.html', flores,name="flores"),
+    path('macetero.html', macetero,name="macetero"),
+    path('tierradehoja.html', tierradehoja,name="tierradehoja"),
 ]
