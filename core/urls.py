34fef3d2605f104dfd541d9879f4959a91e7b2ed
jord.tapiar <jord.tapiar@duocuc.cl>
2022-06-06 18:19:44 -0400

    cambios
    urls y views

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import home
+from .views import home, macetero
+from .views import contacto
+from .views import flores
+from .views import macetero
+from .views import tierradehoja
 
 urlpatterns = [
     path('', home,name="home"),
+    path('', contacto,name="contacto"),
+    path('', flores,name="flores"),
+    path('', macetero,name="macetero"),
+    path('', tierradehoja,name="tierradehoja")
 ]
--- core/views.py
+++ core/views.py
@@ -5,3 +5,19 @@ from django.shortcuts import render
 def home(request):
     
     return render(request, 'core/index.html')
+
+def contacto(request):
+    
+    return render(request, 'core/contacto.html')
+
+def flores(request):
+    
+    return render(request, 'core/flores.html')
+
+def macetero(request):
+    
+    return render(request, 'core/macetero.html')
+
+def tierradehoja(request):
+    
+    return render(request, 'core/tierradehoja.html')
