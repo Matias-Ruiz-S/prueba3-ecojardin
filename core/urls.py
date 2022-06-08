900ae04a4cc441fbd348faef694548deccb2d56d
Matías Ruiz <ma.ruizsa@duocuc.cl>
2022-06-08 18:52:28 -0400

    Varios cambios de detalles
    Para que funcione con django

--- core/urls.py
+++ core/urls.py
@@ -4,11 +4,24 @@ from .views import contacto
 from .views import flores
 from .views import macetero
 from .views import tierradehoja
+from .views import flo001, flo002, flo003, flo004, mac001, mac002, mac003, mac004, tie001, tie002, tie003, tie004
 
 urlpatterns = [
     path('', home,name="home"),
-    path('contacto.html', contacto,name="contacto"),
-    path('flores.html', flores,name="flores"),
-    path('macetero.html', macetero,name="macetero"),
-    path('tierradehoja.html', tierradehoja,name="tierradehoja"),
+    path('contacto', contacto,name="contacto"),
+    path('flores', flores,name="flores"),
+    path('maceteros', macetero,name="macetero"),
+    path('tierradehoja', tierradehoja,name="tierradehoja"),
+    path('flores/001', flo001,name="flo001"),
+    path('flores/002', flo002,name="flo002"),
+    path('flores/003', flo003,name="flo003"),
+    path('flores/004', flo004,name="flo004"),
+    path('maceteros/001', mac001,name="mac001"),
+    path('maceteros/002', mac002,name="mac002"),
+    path('maceteros/003', mac003,name="mac003"),
+    path('maceteros/004', mac004,name="mac004"),
+    path('tierradehoja/001', tie001,name="tie001"),
+    path('tierradehoja/002', tie002,name="tie002"),
+    path('tierradehoja/003', tie003,name="tie003"),
+    path('tierradehoja/004', tie004,name="tie004"),
 ]
--- core/views.py
+++ core/views.py
@@ -33,3 +33,51 @@ def tierradehoja(request):
     }
     
     return render(request, 'core/tierradehoja.html', datos)
+
+def flo001(request):
+    
+    return render(request, 'core/flo001.html')
+
+def flo002(request):
+    
+    return render(request, 'core/flo002.html')
+
+def flo003(request):
+    
+    return render(request, 'core/flo003.html')
+
+def flo004(request):
+    
+    return render(request, 'core/flo004.html')
+
+def mac001(request):
+    
+    return render(request, 'core/mac001.html')
+
+def mac002(request):
+    
+    return render(request, 'core/mac002.html')
+
+def mac003(request):
+    
+    return render(request, 'core/mac003.html')
+
+def mac004(request):
+    
+    return render(request, 'core/mac004.html')
+
+def tie001(request):
+    
+    return render(request, 'core/tie001.html')
+
+def tie002(request):
+    
+    return render(request, 'core/tie002.html')
+
+def tie003(request):
+    
+    return render(request, 'core/tie003.html')
+
+def tie004(request):
+    
+    return render(request, 'core/tie004.html')
