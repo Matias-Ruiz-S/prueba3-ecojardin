8afbdd0d1673d2b16327c425cd8ff9fb8b2e6cae
jordtapiar <104394688+jordtapiar@users.noreply.github.com>
2022-06-08 00:27:45 -0400

    datos
    colocación de datos en cada categoría

--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import flores
+from .models import macetero
+from .models import tierradehoja
 
 # Create your views here.
 
@@ -11,13 +14,24 @@ def contacto(request):
     return render(request, 'core/contacto.html')
 
 def flores(request):
-    
-    return render(request, 'core/flores.html')
+    flores = flores.objects.all()
+    datos ={
+        'flores': flores
+    }
+    return render(request, 'core/flores.html', datos)
 
 def macetero(request):
+    macetero = macetero.objects.all()
+    datos ={
+        'macetero': macetero
+    }
     
-    return render(request, 'core/macetero.html')
+    return render(request, 'core/macetero.html', datos)
 
 def tierradehoja(request):
+    tierradehoja= tierradehoja.objects.all()
+    datos ={
+        'tierra de hoja': tierradehoja
+    }
     
-    return render(request, 'core/tierradehoja.html')
+    return render(request, 'core/tierradehoja.html', datos)
