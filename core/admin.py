9806dbd527d18c07a0d82a1523445d2359b0b0a5
jordtapiar <104394688+jordtapiar@users.noreply.github.com>
2022-06-08 00:17:25 -0400

    admin
    crear tablas

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Categoria, flores, macetero, tierradehoja
 
 # Register your models here.
+
+admin.site.register(Categoria)
+admin.site.register(flores)
+admin.site.register(macetero)
+admin.site.register(tierradehoja)
