e301916add2d1a075bd32d0ef72955f6f0b57b61
jordtapiar <104394688+jordtapiar@users.noreply.github.com>
2022-06-08 00:11:12 -0400

    creacion de modelos
    categoria, flores, ect

--- core/models.py
+++ core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    idCategoria =models.IntegerField(primary_key=True, verbose_name='Id de categoria')
+    nombreCategoria = models.CharField(max_length=50, verbose_name='Nombre de la categoria')
+
+
+def __str__(self):
+    return self.nombreCategoria
+
+
+class flores(models.Model):
+    sku = models.CharField(max_length=12, primary_key=True, verbose_name='SKU')
+    nombreProducto = models.CharField(max_length=20, verbose_name='Nombre del producto')
+    precio = models.CharField(max_length=20, verbose_name='Precio')
+    Stock = models.CharField(max_length=20, null=True, blank=True, verbose_name='Stock disponible')
+    Categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+class macetero(models.Model):
+    sku = models.CharField(max_length=12, primary_key=True, verbose_name='SKU')
+    nombreProducto = models.CharField(max_length=20, verbose_name='Nombre del producto')
+    precio = models.CharField(max_length=20, verbose_name='Precio')
+    Stock = models.CharField(max_length=20, null=True, blank=True, verbose_name='Stock disponible')
+    Categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+class tierradehoja(models.Model):
+    sku = models.CharField(max_length=12, primary_key=True, verbose_name='SKU')
+    nombreProducto = models.CharField(max_length=20, verbose_name='Nombre del producto')
+    precio = models.CharField(max_length=20, verbose_name='Precio')
+    Stock = models.CharField(max_length=20, null=True, blank=True, verbose_name='Stock disponible')
+    Categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
