7f52a651dbd6d136bb712f11716707318ddecf1f
Matías Ruiz <ma.ruizsa@duocuc.cl>
2022-07-01 04:17:57 -0400

    API rest desarrollado
    Añadi las caracteristicas de get/post, update/delete/get y seguridad con token

--- prueba3/settings.py
+++ prueba3/settings.py
@@ -40,7 +40,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'core',
     'rest_producto',
+    'rest_framework.authtoken',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- rest_producto/urls.py
+++ rest_producto/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from rest_producto.views import lista_productos
+from rest_producto import views
+from rest_producto.viewslogin import login
 
 urlpatterns = [
     path('lista_productos', lista_productos, name="lista_productos"),
+    path('detalle_producto/<id>', views.detalle_producto, name="detalle_producto"),
+    path('login', login, name="login"),
 ]
